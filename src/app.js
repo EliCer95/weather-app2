71ce82d5af12ae456d07609c13f09f3e766ff585
EliCer95 <eliskacerna95@gmail.com>
2021-09-16 21:45:33 +0200

    Added temperature API

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperature = document.querySelector("#temperature");
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = Math.round(response.data.main.humidity);
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "5af297a6d7993b7bb3c2ec51eeeaccd4";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
